2ff0e678042a5a390dd13de8d56a533c8861f76d
Anthony McLeod <mcleod333@gmail.com>
2018-02-22 22:42:39 -0500

    fixed validate path

--- api/validate/validateRoutes.js
+++ api/validate/validateRoutes.js
@@ -8,7 +8,7 @@ var authCheck = [auth.decodeToken];
 router.param('phone_number', controller.params);
 
 router.route('/decode').post(controller.validateCode);
-router.route('/code/phone_number').get(controller.sendCode);
+router.route('/code/:phone_number').get(controller.sendCode);
 
 // this will run if there is no email in the request
 router.route('*').all(controller.badPath);
