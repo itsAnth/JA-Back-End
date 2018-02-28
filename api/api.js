33ae35c160b5967b066da594e7c48abd56573479
Anthony McLeod <mcleod333@gmail.com>
2018-02-27 22:30:59 -0500

    added terms and online test

--- api/api.js
+++ api/api.js
@@ -5,11 +5,23 @@ var logger = require('../util/logger');
 router.use('/sms', require('./sms/smsRoutes'));
 router.use('/validate', require('./validate/validateRoutes'));
 
+router.use('/online').get(function(req, res,) {
+	var oRes = {
+		success: true,
+		status: "online"
+	};
+	var sResponse = JSON.stringify(oRes);
+	res.type('json');
+	res.status(200).send(sResponse);
+});
 router.route('/').get(function(req, res) {
 	res.sendFile(path.join(__dirname+'/../html/routes.html'));
 });
 router.route('/database').get(function(req, res) {
 	res.sendFile(path.join(__dirname+'/../html/database.html'));
+});
+router.route('/terms').get(function(req, res) {
+	res.sendFile(path.join(__dirname+'/../html/termsfeed-terms-conditions-html-english.html'));
 });
 router.route('/favicon.ico').get(function(req, res) {
     res.status(204);
